6141290dd0fcddf116a5d7f9dbbe51788c58901d
harisanker10 <harisanker10@gmail.com>
2024-07-09 19:55:15 +0530

    cloudflare functions middleware1

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,8 @@
 export async function onRequest(context) {
   try {
     console.log({ context });
+    const task = await context.env.sort_visualizer.get("name");
+    console.log({ task });
     return await context.next();
   } catch (err) {
     return new Response(`${err.message}\n${err.stack}`, { status: 500 });
